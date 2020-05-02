011428c634889b463089ec26d9f55e1d726f40bf
gavinbm <gaving1226@gmail.com>
2020-05-01 23:23:19 -0400

    getting there kinda

--- ksalf/home.py
+++ ksalf/home.py
@@ -1,10 +1,18 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        user = request.form["nm"]
+        return redirect(url_for("user", name=user))
+    else:
+        return render_template("login.html")
 
 @app.route("/<name>")
 def user(name):
