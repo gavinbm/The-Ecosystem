6ab0dfcbc623d7591a37bf8dca085e40241cfa59
gavinbm <gaving1226@gmail.com>
2020-09-11 10:57:20 -0400

    adding login and account creation to home page

---
+++ flask/helpers.py
@@ -0,0 +1,45 @@
+from flask import Flask, redirect, url_for, render_template, request, session, flash
+import json
+
+app = Flask(__name__)
+app.secret_key = "howdy"
+
+with open("accounts.json") as f:
+    Users = json.load(f)
+
+# Special chars for passwords
+Specials = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-']
+
+# Ensures a secure password
+def check_password(Specials, password):
+    if len(password) < 8:
+        return False
+    if not any(char.isdigit() for char in password) or not any(char in Specials for char in password):
+        return False
+    if not any(char.isupper() for char in password) or not any(char.islower() for char in password):
+        return False
+    else:
+        return True
+
+# Checks if a username already exists
+def check_username(Users, name):
+    check = True
+    for user in Users:
+        if user["name"] == name:
+            check = False
+    return check
+
+# Finds a user
+def find_user(Users, name, password):
+    check = False
+    for user in Users:
+        if user["name"] == name and user["password"] == password:
+            check = True
+    return check
+
+# Creates a user
+def create_user(name, password):
+    user = {'name':name, 'password':password, 'games':[]}
+    Users.append(user)
+    with open("accounts.txt", "w") as fout:
+            json.dump(Users, fout)
--- flask/home.py
+++ flask/home.py
@@ -1,55 +1,34 @@
-from flask import Flask, redirect, url_for, render_template, request
-import json
+from helpers import *
 
-app = Flask(__name__)
-
-with open("accounts.json") as f:
-    Users = json.load(f)
-
-@app.route("/")
+@app.route("/", methods = ["POST", "GET"])
 def home():
-    return render_template("index.html")
+    render_template("index.html")
+    if request.method == "POST":
+        name = request.form["nm"]
+        password = request.form["pw"]
+        if find_user(Users, name, password):
+            flash("Welcome Back Friend :D")
+        else:
+            if check_username(Users, name) and check_password(Specials, password):
+                create_user(name, password)
+            else:
+                return render_template("index.html")
+    else:
+        return render_template("index.html")
 
 @app.route("/make", methods=["POST", "GET"])
 def make():
      if request.method == "POST":
          name = request.form["nm"]
          password = request.form["pw"]
-         pass_conf = request.form["pw2"]
          if check_password(Specials, password, pass_conf) and check_username(Users, name):
-             user = {"name": name, "password": password, "status": 1}
+             user = {"name": name, "password": password, "status": 1, "gamesplayed":[]}
              Users.append(user)
          with open("accounts.txt", "w") as fout:
              json.dump(Users, fout)
          return redirect(url_for("user", name=name))
      else:
          return render_template("make.html")
-
-@app.route("/login", methods=["POST", "GET"])
-def login():
-    if request.method == "POST":
-        name = request.form["nm"]
-        password = request.form["pw"]
-        check = False
-        for user in Users:
-            if user["name"] == name and user["password"] == password:
-                check = True
-                user["status"] = 1
-                with open("accounts.txt", "w") as fout:
-                    json.dump(Users, fout)
-                return redirect(url_for("user", name=name))
-        if not check:
-            return render_template("login.html")
-    else:
-        return render_template("login.html")
-
-@app.route("/<name>")
-def user(name):
-    return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="root"))
 
 if __name__ == "__main__":
     app.run(debug=True)
